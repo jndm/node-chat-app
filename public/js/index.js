24de06c2466f668bddcdab4b0835f2b03b3bb55e
Joonas Meriläinen <joonas.merilainen@gofore.com>
2017-11-14 17:04:12 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,12 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'kari',
-        text: 'create this message'
-    });
-
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -13,14 +13,13 @@ app.use(express.static(path.join(__dirname, '../public')));
 io.on('connection', (socket) => {
     console.log('New user connected!');
 
-    socket.emit('newMessage', {
-        from: 'jari',
-        text: 'new text',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('Created message', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', (socket) => {
