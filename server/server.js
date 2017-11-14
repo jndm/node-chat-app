43a6fcc1f517598c72e7c18956701d50511ba9d3
Joonas Meriläinen <joonas.merilainen@gofore.com>
2017-11-14 15:35:28 +0200

    little fix on port

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
