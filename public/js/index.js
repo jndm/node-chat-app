00edc013e1919ba4aa348bb75f4a7431d9a0a166
Joonas Meriläinen <joonas.merilainen@gofore.com>
2017-11-15 13:09:15 +0200

    Added css and fixed some ux-elements

--- public/js/index.js
+++ public/js/index.js
@@ -27,11 +27,13 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function(e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function(data) {
-        console.log(data);
+        messageTextbox.val('');
     });
 });
 
@@ -41,12 +43,17 @@ locationButton.on('click', function(e) {
         return alert('Geolocation not supported by your browser.');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send location');
+
         socket.emit('createLocationMessage', {
             lat: position.coords.latitude,
             lng: position.coords.longitude
         });
     }, function() {
+        locationButton.removeAttr('disabled').text('Send location');;
         alert('Unable to fetch location.')
     });
 });
